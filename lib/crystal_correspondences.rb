96d675a24c7ab50c6d2b9b1a7fb63b8851889b2f
Audrea Cook <audreacwork@gmail.com>
2019-08-05 13:36:29 -0600

    Defined call and menu methods.

--- lib/crystal_correspondences.rb
+++ lib/crystal_correspondences.rb
@@ -1,2 +1,3 @@
+# acts as environment... requires all other dependencies
 require_relative "../lib/crystal_correspondences/version"
 require_relative "../lib/crystal_correspondences/cli"
--- lib/crystal_correspondences/cli.rb
+++ lib/crystal_correspondences/cli.rb
@@ -1,6 +1,39 @@
 # This is my CLI controller
 class CrystalCorrespondences::CLI
   def call
-    puts "Welcome, witch!"
+    puts "Welcome, friend!"
+    menu
   end
+
+  def menu
+    puts "To see a list of crystals, type 'list'."
+    puts "To see crystals by their purpose, type 'purpose'."
+    puts "To see crystals by their color, type 'color.'"
+    puts "To exit, type 'exit'."
+    input = nil
+    while input != 'exit'
+      input = gets.strip
+      case input
+      when list
+        list
+      when purpose
+        purpose
+      when color
+        color
+      else
+        puts "Try one of these options:"
+        menu
+      end
+    end
+  end
+
+  # - show welcome message
+  # - show menu
+  # - user types crystals
+  # - show list of crystals
+  # - user chooses crystal
+  # - show crystal correspondences
+  # - show menu
+  # - user types exit
+  # - show goodbye message
 end
