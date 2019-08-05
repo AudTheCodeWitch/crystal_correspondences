b8091db5a5ac04fae0cd46ae3f0c354831c59617
Audrea Cook <audreacwork@gmail.com>
2019-08-05 14:52:43 -0600

    Moved initialize and @@all to correct class.

--- lib/purposes.rb
+++ lib/purposes.rb
@@ -1,12 +1,3 @@
 class Purposes
-# Hold all the crystals and properties
-# Properties: name, color (added via color class), purpose (added via purpose class), healing properties, gemstone properties
-@@all = []
-  def initialize
-    @@all << self
-  end
 
-  def self.all
-    @@all
-  end
 end
