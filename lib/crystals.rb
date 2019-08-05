00e9837e35a901cf1054db58488d753024de3452
Audrea Cook <audreacwork@gmail.com>
2019-08-05 14:55:41 -0600

    Added attr_accessors

--- lib/crystals.rb
+++ lib/crystals.rb
@@ -1,8 +1,14 @@
 class Crystals
 # Hold all the crystals and properties
 # Properties: name, color (added via color class), purpose (added via purpose class), healing properties, gemstone properties
+attr_accessor :name, :color, :purpose, :healing_properties, :gemstone_properties
   @@all = []
-  def initialize
+  def initialize(name, color = nil, purpose = nil, healing_properties = nil, gemstone_properties = nil)
+    @name = name
+    @color = color
+    @purpose = purpose
+    @healing_properties = healing_properties
+    @gemstone_properties = gemstone_properties
     @@all << self
   end
 
