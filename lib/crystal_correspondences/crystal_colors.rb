8f90ea6e1282e8ca93af6f25b0b41d6648396256
Audrea Cook <audreacwork@gmail.com>
2019-08-07 15:06:39 -0600

    Changed from def to class. Added new_color and new_purpose method stubs.

--- lib/crystal_correspondences/crystal_colors.rb
+++ lib/crystal_correspondences/crystal_colors.rb
@@ -1,3 +1,9 @@
-def CrystalColors
+require 'pry'
+class CrystalColors
   # Use this to assign colors to crystals and crystals to colors
+  # I need to create a new color instance and add the crystal to it. If one already exists, just add the crystal.
+  def new_color(name)
+    Color.new(name) # unless it's already there
+  end
+  # I need to push the new color to the crystal's purposes array.
 end
--- lib/crystal_correspondences/crystal_purposes.rb
+++ lib/crystal_correspondences/crystal_purposes.rb
@@ -1,5 +1,8 @@
-def CrystalPurposes
+class CrystalPurposes
   # Use this to assign purposes to crystals and crystals to purposes
   # I need to create a new purpose instance and add the crystal to it. If one already exists, just add the crystal.
+  def new_purpose(name)
+    Purpose.new(name) # unless it's already there
+  end
   # I need to push the new purpose to the crystal's purposes array.
 end
