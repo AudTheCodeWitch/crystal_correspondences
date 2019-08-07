9dd12bcac99d00a0fd35a2c63c13510ffd2e4f50
Audrea Cook <audreacwork@gmail.com>
2019-08-07 11:38:06 -0600

    Edited attr_accessors. Edited initialize. Created add_color and add_purpose methods.

--- lib/crystal_scraper.rb
+++ lib/crystal_scraper.rb
@@ -14,14 +14,9 @@ class CrystalScraper
     crystal_url = card.css('h3').css('a').attribute('href').value
     crystal_doc = Nokogiri::HTML(open(crystal_url))
     # name: doc.css('.card.flex-card.horizontal.per-row-1-2').first.css('h3').text
-    # color: crystal_doc.css('.entry-content').css('div')[-2].text.split('C
-    # olor:')[1].strip
-    # purpose: crystal_doc.css('.entry-content').css('div')[-2].text.split('C
-    # olor:')[0].split('Uses:')[1].strip
-    # healing_properties: crystal_doc.css('.entry-content').css('p')[0].text
-    # gemstone_properties: crystal_doc.css('.entry-content').css('p')[-3..-2].text
-    # crystal_url: doc.css('.card.flex-card.horizontal.per-row-1-2').first.css('h3').css('a').att
-    # ribute('href').value
+    # color: crystal_doc.css('.entry-content').css('div')[-2].text.split('Color:')[1].strip
+    # purpose: crystal_doc.css('.entry-content').css('div')[-2].text.split('Color:')[0].split('Uses:')[1].strip
+    # crystal_url: doc.css('.card.flex-card.horizontal.per-row-1-2').first.css('h3').css('a').attribute('href').value
 
     binding.pry
   end
--- lib/crystals.rb
+++ lib/crystals.rb
@@ -2,12 +2,23 @@ require 'pry'
 class Crystals
 # Hold all the crystals and properties
 # Properties: name, color, purpose, url
-attr_accessor :name, :crystal_url, :color, :purpose
+attr_accessor :name, :crystal_url, :colors, :purposes
   @@all = []
   def initialize(name)
     @name = name
+    @crystal_url = crystal_url
+    @colors = []
+    @purposes = []
     @@all << self
   end
+
+def add_color(color)
+  @colors << color
+end
+
+def add_purpose(purpose)
+  @purposes << purpose
+end
 
   def self.all
     @@all
