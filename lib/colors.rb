3a007b3d34170ff2502702c6407d2664c991df35
Audrea Cook <audreacwork@gmail.com>
2019-08-07 13:56:23 -0600

    Rough colors class

--- lib/colors.rb
+++ lib/colors.rb
@@ -1,3 +1,16 @@
 class Colors
+  attr_accessor :name
+  attr_reader :crystals
+  @@all = []
+
+  def initialize
+    @name = name
+    @crystals = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
