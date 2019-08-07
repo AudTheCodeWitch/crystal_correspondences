29fd053c26b1ed838f3a40c4790cf2c556f33387
Audrea Cook <audreacwork@gmail.com>
2019-08-07 15:56:36 -0600

    Built out crystal_purpose joining class.

--- lib/crystal_correspondences/crystal_purposes.rb
+++ lib/crystal_correspondences/crystal_purposes.rb
@@ -1,8 +1,19 @@
 class CrystalPurposes
-  # Use this to assign purposes to crystals and crystals to purposes
+  # Use this to assign purposes to crystals and crystals to purposes - joining model
   # I need to create a new purpose instance and add the crystal to it. If one already exists, just add the crystal.
-  def new_purpose(name)
-    Purpose.new(name) # unless it's already there
-  end
   # I need to push the new purpose to the crystal's purposes array.
+  #
+  attr_accessor :crystal, :purpose
+  @@all = []
+
+  def initialize(crystal, purpose)
+    @crystal = crystal
+    @purpose = purpose
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
