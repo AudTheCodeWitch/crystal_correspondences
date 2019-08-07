7284fd90565ae7254902df1cecce4f82d30feb1a
Audrea Cook <audreacwork@gmail.com>
2019-08-07 16:10:01 -0600

    Built initialize and self.all methods

--- lib/crystal_correspondences/crystal_colors.rb
+++ lib/crystal_correspondences/crystal_colors.rb
@@ -2,8 +2,17 @@ require 'pry'
 class CrystalColors
   # Use this to assign colors to crystals and crystals to colors
   # I need to create a new color instance and add the crystal to it. If one already exists, just add the crystal.
-  def new_color(name)
-    Color.new(name) # unless it's already there
+  attr_accessor :crystal, :color
+  @@all = []
+
+  def initialize(crystal, color)
+    @crystal = crystal
+    @color = color
+    @@all << self
   end
-  # I need to push the new color to the crystal's purposes array.
+
+  def self.all
+    @@all
+  end
+
 end
