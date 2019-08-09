8f8bb9c1656c9368c67f6078324bf83fd7b36b5c
Audrea Cook <audreacwork@gmail.com>
2019-08-09 11:17:02 -0600

    Removed extra method. Fixed spacing.

--- lib/crystal_correspondences/purpose.rb
+++ lib/crystal_correspondences/purpose.rb
@@ -14,26 +14,22 @@ class Purpose
   def save
     self.class.all << self
   end
+
   def self.find_or_create(name)
     @@all.find do |p|
       p.name == name
     end || create(name)
   end
+
   def self.create(name)
     purpose = new(name)
     purpose.save
     purpose
   end
 
-  # def add_crystal(crystal)
-  #   CrystalPurposes.new(crystal, self)
-  # end
-  #
-
   def self.crystals(p)
     CrystalPurposes.all.select do |crystal|
       crystal.purpose == p
     end.map{ |cp| cp.crystal}
   end
-
 end
