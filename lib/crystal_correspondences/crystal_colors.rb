adf3a0fb1b25ef98e402be77a31a0213b6794c81
Audrea Cook <audreacwork@gmail.com>
2019-08-09 11:15:51 -0600

    Removed comments. Fixed spacing.

--- lib/crystal_correspondences/crystal_colors.rb
+++ lib/crystal_correspondences/crystal_colors.rb
@@ -1,7 +1,4 @@
-require 'pry'
 class CrystalColors
-  # Use this to assign colors to crystals and crystals to colors
-  # I need to create a new color instance and add the crystal to it. If one already exists, just add the crystal.
   attr_accessor :crystal, :color
   @@all = []
 
@@ -23,5 +20,4 @@ class CrystalColors
     color.save
     color
   end
-
 end
