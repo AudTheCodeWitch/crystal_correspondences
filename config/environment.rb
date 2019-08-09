ad07d4465a11409e58f4cb88a39038eefa09e64e
Audrea Cook <audreacwork@gmail.com>
2019-08-09 11:06:45 -0600

    Removed comments

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,3 @@ require 'open-uri'
 require 'pry'
 require 'require_all'
 require_all './lib/crystal_correspondences'
-
-# test = Scraper.new
-# test.get_crystals
-# Crystal.all = Crystal.all[0..10]
-# test.scrape_properties
-# binding.pry
